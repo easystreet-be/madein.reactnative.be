8dbe90697e66c3967f06172c021917ff2ca546b1
Robin Claes <robin@easystreet.be>
2023-09-04 10:50:46 +0200

    Fix project screenshot height

--- src/components/project/detail/ProjectScreenshots.tsx
+++ src/components/project/detail/ProjectScreenshots.tsx
@@ -1,30 +1,28 @@
-import style from '@/components/project/detail/ProjectScreenshots.module.scss'
-import { translations } from '@/util/locale/localization'
-import Image from 'next/image'
+import style from "@/components/project/detail/ProjectScreenshots.module.scss";
+import { translations } from "@/util/locale/localization";
+import Image from "next/image";
 
 interface ProjectScreenshotsProps {
-    mockupPrimaryUrl?: string,
-    mockupSecondaryUrl?: string,
-    screenshotUrls: Array<string>,
+  mockupPrimaryUrl?: string;
+  mockupSecondaryUrl?: string;
+  screenshotUrls: Array<string>;
 }
 
 const ProjectScreenshots = (props: ProjectScreenshotsProps) => {
-    if (props.screenshotUrls.length == 0) return (<></>)
-    return (
-        <div className={style.container}>
-            {
-                props.screenshotUrls.map((url, index) => (
-                    <Image
-                        key={index}
-                        width={180}
-                        height={320}
-                        alt={`${translations.alt_screenshot} ${index + 1}`}
-                        src={url} />
-                ))
-            }
-        </div>
-    )
-}
+  if (props.screenshotUrls.length == 0) return <></>;
+  return (
+    <div className={style.container}>
+      {props.screenshotUrls.map((url, index) => (
+        <Image
+          key={index}
+          width={180}
+          height={389}
+          alt={`${translations.alt_screenshot} ${index + 1}`}
+          src={url}
+        />
+      ))}
+    </div>
+  );
+};
 
-
-export default ProjectScreenshots
+export default ProjectScreenshots;
